dae55bbfd1332b7635cf43b4c6bbf0c6e0cb4761
Cleison-Santos-Carvalho <cleison.carvalho.santos@gmail.com>
2025-03-13 09:44:43 -0300

    Saida de Dados usando o comando print

---
+++ UsandoPrint.java
@@ -0,0 +1,10 @@
+public class UsandoPrint {
+
+    public static void main(String[] args) {
+
+        System.out.print("Bom dia!");
+        System.out.print("Boa tarde!");
+        System.out.print("Boa noite");
+
+    }
+}
