ef20bc828948d906c7f1a76ec81b210d03c83e7a
Cleison-Santos-Carvalho <cleison.carvalho.santos@gmail.com>
2025-03-13 10:27:47 -0300

    Saida de dados usando o comando println

---
+++ UsandoPrintln.java
@@ -0,0 +1,10 @@
+public class UsandoPrintln {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bom dia!");
+        System.out.println("Boa tarde!");
+        System.out.println("Boa noite!");
+
+    }
+}
