5498c1ba09fdbdd140e55b91da383ec6cfa3b628
Cleison-Santos-Carvalho <cleison.carvalho.santos@gmail.com>
2025-03-13 11:19:58 -0300

    Saida de dados usando o print e concatenando string e variaveis

---
+++ UsandoConcatenacao.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public class UsandoConcatenacao {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        double x = 10.35784;
+
+        System.out.print("RESULTADO = " + x + " METROS ");
+
+
+
+    }
+
+
+
+}
