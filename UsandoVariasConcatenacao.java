03e90619078a328c2020490e30df14b8d078dc27
Cleison-Santos-Carvalho <cleison.carvalho.santos@gmail.com>
2025-03-13 11:53:30 -0300

    Acrescentado saida com o comando printf e varios playceholders

--- UsandoVariasConcatenacao.java
+++ UsandoVariasConcatenacao.java
@@ -9,5 +9,12 @@ public class UsandoVariasConcatenacao {
         double x = 10.35784;
 
         System.out.printf("RESULTADO = %.2f metros%n", x);
+
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+
+        System.out.printf("%s tem %d anos e ganha %.2f reais%n", nome, idade, renda);
+
     }
 }
